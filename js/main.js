c314a4bc53be47ac9f4efd47012902487232ab7f
erine tang <erine tang>
2017-03-22 17:20:50 +0800

    Site updated: 2017-03-22 17:20:50

--- js/main.js
+++ js/main.js
@@ -10238,14 +10238,13 @@ if ( !noGlobal ) {
 }
 
 
+
 $('a.gallery').featherlightGallery({
-		previousIcon: '«',
-		nextIcon: '»',
-		galleryFadeIn: 300,
-
-		openSpeed: 300
+		previousIcon: '&#9664;',     /* Code that is used as previous icon */
+		nextIcon: '&#9654;',         /* Code that is used as next icon */
+		galleryFadeIn: 100,          /* fadeIn speed when slide is loaded */
+		galleryFadeOut: 300          /* fadeOut speed before slide is loaded */
 	});
-
 
 
 return jQuery;
